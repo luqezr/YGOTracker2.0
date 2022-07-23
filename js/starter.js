9abe29ee67addb008beb4bdb4bd8baddc189b4c2
Lucas <46754282+luqezr@users.noreply.github.com>
2022-07-23 17:05:32 -0300

    changes
    added searchcards

--- js/starter.js
+++ js/starter.js
@@ -1,22 +1,29 @@
 // var initializations
 var lang;
 var allCards;
+var ygoorgCard;
+var query;
+var filteredQueryResults;
 
 // start webpage
 window.onload = startWebPage();
 
 function startWebPage() {
-  if (lang == undefined) {
+  if (lang == undefined || lang == null) {
     // setting default language to english...
-    lang = "eng";
+    lang = "en";
     // hacer un query a la db de todas las cartas
   }
   queryYGOPD();
 }
 
+// Query Yugiohprodeck DB
+
 async function queryYGOPD() {
   // https://db.ygoprodeck.com/api/v7/cardinfo.php
-  const response = await fetch("https://db.ygoprodeck.com/api/v7/cardinfo.php")
+  const response = await fetch(
+    "https://db.ygoprodeck.com/api/v7/cardinfo.php?&misc=yes"
+  )
     .then((response) => response.json())
     .then((data) => {
       allCards = data;
@@ -30,13 +37,15 @@ async function queryYGOPD() {
     });
 }
 
-async function queryYGOrg() {
+// Query YgoOrganization
+
+async function queryYGOrg(card) {
   // https://db.ygoprodeck.com/api/v7/cardinfo.php
-  const response = await fetch("https://db.ygoprodeck.com/api/v7/cardinfo.php")
+  await fetch(`https://db.ygorganization.com/data/card/${card}`)
     .then((response) => response.json())
     .then((data) => {
-      allCards = data;
-      console.log(allCards.data);
+      ygoorgCard = data;
+      console.log(ygoorgCard);
     })
 
     .catch((error) => {
@@ -44,4 +53,22 @@ async function queryYGOrg() {
       return;
       // Code for handling the error
     });
+}
+
+// SORTERS
+
+function sortBy(value) {
+  allCards.data.sort(function (a, b) {
+    return a.id - b.id;
+  });
+}
+
+// SEARCHERS
+
+function searchCardsByNameOrDescription(value) {
+  query = value;
+  filteredQueryResults = allCards.data.filter((card) =>
+    `${card.name} ${card.desc}`.includes(query)
+  );
+  console.log(filteredQueryResults);
 }
